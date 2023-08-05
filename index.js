a0a8e13b87141e68b23e4c7c2919274f8a2d09e0
ingenriquelopez <ingeniero.enriquelopez@gmail.com>
2023-08-05 13:33:34 -0600

    env

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const PORT = 3001;
+require('dotenv').config();
+const {PORT}= process.env;
+
+const { db } = require('./src/db.js');
+
+
+const cors         = require('cors');
+const express      = require('express');
+const fileUpload   = require('express-fileupload');
+
 const express = require('express');
 const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(fileUpload( {
+  createParentPath:true  
+}));
+
 
 server.get('/',(req,res)=> {
     res.send('hola perros')
 });
 
 
-server.listen( PORT, ()=> {
-    console.log(`listening on port: ${PORT}`)
-    return(`listening on port: ${PORT}`)
-})
+
+
+
+
+/* db.sync({ force: true })
+  .then(()=>{    
+    console.log("DataBase sync");
+  }); */
+
+server.listen(PORT,()=> {
+    console.log(`Listening on port. ${PORT} `);
+});
