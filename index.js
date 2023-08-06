53565762d3933f58df4ebeff10b8e09799c4ff3c
ingenriquelopez <ingeniero.enriquelopez@gmail.com>
2023-08-05 18:25:21 -0600

    seq

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const { DB_DIALECT, DB_NAME, DB_USER, DB_PASSWORD, DB_HOST,DB_PORT} = process.en
 
 const {Sequelize }            = require('sequelize');
 
-const sequelize = new Sequelize(`${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, 
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+});
+
+console.log('______________________');
+console.log(sequelize)
+console.log('______________________');
+/* const sequelize = new Sequelize(`${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, 
   { 
     logging: false,
     native:false
   }
-  )
+  ) */
+
+  
 
 const cors         = require('cors');
 const express      = require('express');
@@ -33,10 +45,10 @@ server.get('/',(req,res)=> {
 
 
 
-/* db.sync({ force: true })
+sequelize.sync({ force: true })
   .then(()=>{    
     console.log("DataBase sync");
-  }); */
+  }); 
 
 server.listen(PORT,()=> {
     console.log(`Listening on port. ${PORT} `);
